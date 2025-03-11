68457dc3db448de8124a86d2dc05d118f189c4ce
MichAW-006 <147823444+MichAW-006@users.noreply.github.com>
2025-03-11 10:09:00 +0000

    Update play2.py

--- play2.py
+++ play2.py
@@ -9,9 +9,9 @@ pygame.display.set_caption("Life Simulator")
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 header_rect = pygame.Rect(0, 0, 800, 50)
-conversate_button = pygame.Rect(500, 400, 200, 50)
-ask_for_money_button = pygame.Rect(500, 300, 200, 50)
-back_button=Button(pygame.image.load('back.png').convert_alpha(),0,0,0.13)
+ask_to_be_friends_button = pygame.Rect(500, 400, 200, 50)
+ask_to_date_button = pygame.Rect(500, 300, 200, 50)
+back_button=Button(pygame.image.load('back.PNG').convert_alpha(),0,0,0.13)
 npc_buttons = [pygame.Rect(20, 80 + (i * 50), 300, 40) for i in range(len(Game.npcs))]
 selected_npc = None
 
@@ -29,7 +29,7 @@ def draw_relationships_screen():
     i=0
     for npc in npc_buttons:
         pygame.draw.rect(screen, Game.colour_1, npc)
-        screen.blit(Game.fonts[0].render(f"{Game.npcs.relationships[i].name} {Game.player.relationships[i].surname} ", True, BLACK), (npc.x + 10, npc.y + 10))
+        screen.blit(Game.fonts[0].render(f"{Game.npcs[i].name} {Game.npcs[i].surname} ", True, BLACK), (npc.x + 10, npc.y + 10))
         i+=1
 
     # Display stats if an NPC is selected
@@ -45,11 +45,12 @@ def display_stats(npc):
     screen.blit(Game.fonts[1].render("Relationship", True, BLACK), (655, 180))
     screen.blit(Game.fonts[1].render("Money", True, BLACK), (655, 210))
     draw_progress_bar(440, 210, npc.money/100, 100)
-    pygame.draw.rect(screen, Game.colour_1, conversate_button)
-    screen.blit(Game.fonts[0].render("Have a chat", True, WHITE), (conversate_button.x+10, conversate_button.y + 15))
-    pygame.draw.rect(screen, Game.colour_1, ask_for_money_button)
-    screen.blit(Game.fonts[0].render("Ask for money", True, WHITE), (ask_for_money_button.x + 10, ask_for_money_button.y + 15))
-    
+    pygame.draw.rect(screen, Game.colour_1, ask_to_be_friends_button)
+    screen.blit(Game.fonts[0].render("Offer to be friends", True, WHITE), (ask_to_be_friends_button.x+10, ask_to_be_friends_button.y + 15))
+    if npc.age>16:
+      pygame.draw.rect(screen, Game.colour_1, ask_to_date_button)
+      screen.blit(Game.fonts[0].render("Ask to date", True, WHITE), (ask_to_date_button.x + 10, ask_to_date_button.y + 15))
+      
 
 
 running = True
@@ -66,11 +67,14 @@ while running:
                     selected_npc = Game.npcs[i]
                     break
                 if selected_npc is not None:
-                    if ask_for_money_button.collidepoint(mouse_pos):
-                        selected_npc.ask_for_money()    
+                    if ask_to_date_button.collidepoint(mouse_pos):
+                        if selected_npc.to_date() is True:
+                          Game.player.relationships.append(selected_npc)
                         Game.actions+=1                   
-                    if conversate_button.collidepoint(mouse_pos):
-                        selected_npc.have_conversation()
+                    if ask_to_be_friends_button.collidepoint(mouse_pos):
+                        if selected_npc.be_friends() is True:
+                          Game.player.relationships.append(selected_npc)
+                        
                         Game.actions+=1 
                         
                                 
